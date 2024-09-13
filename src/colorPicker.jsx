1c5ee6f5bd4875937c2da0d6fc92fdc0c656b93a
blackingg <mudet04@gmail.com>
2024-09-13 19:10:13 +0100

    added colorWheel

--- src/colorPicker.jsx
+++ src/colorPicker.jsx
@@ -1,15 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { SketchPicker } from "react-color";
 import { colorData } from "./colorData";
 
-function ColorPicker({ onColorChange }) {
+function ColorPicker({ onColorChange, theme = "light" }) {
+  const [selectedColor, setSelectedColor] = useState("#ffffff");
+  const [isPickerVisible, setPickerVisible] = useState(false);
+
+  const handleColorChange = (color) => {
+    setSelectedColor(color.hex);
+    onColorChange(color.hex);
+  };
+
+  const togglePickerVisibility = () => {
+    setPickerVisible(!isPickerVisible);
+  };
+
+  // Apply custom styles to SketchPicker based on the current theme
+  const pickerStyles = {
+    default: {
+      picker: {
+        background: theme === "dark" ? "#333" : "#fff",
+        color: theme === "dark" ? "#fff" : "#000",
+      },
+    },
+  };
+
   return (
-    <div className="absolute bottom-20 lg:bottom-10 left-1/2 transform -translate-x-1/2 flex p-2 gap-4 bg-white rounded-2xl shadow-lg">
-      {colorData.map((color) => (
-        <>
+    <div className="absolute bottom-20 lg:bottom-10 left-1/2 transform -translate-x-1/2 flex flex-col gap-4 p-2 bg-white rounded-2xl shadow-lg">
+      {/* Predefined color buttons */}
+      <div className="flex gap-4">
+        {colorData.map((color) => (
           <div
             className="relative group"
             key={color.name}
-            onClick={() => onColorChange(color.hex)}
+            onClick={() => handleColorChange(color)}
           >
             <div
               className="w-10 md:w-12 h-10 md:h-12 rounded-full cursor-pointer shadow-lg"
@@ -19,8 +43,25 @@ function ColorPicker({ onColorChange }) {
               {color.name}
             </div>
           </div>
-        </>
-      ))}
+        ))}
+        <div
+          className={`${
+            isPickerVisible
+              ? "bg-colorWheel bg-black bg-opacity-50 bg-blend-multiply"
+              : "bg-colorWheel"
+          } bg-no-repeat bg-center bg-cover w-10 md:w-12 h-10 md:h-12 rounded-full relative`}
+          onClick={togglePickerVisibility}
+        ></div>
+        {isPickerVisible && (
+          <div className="absolute bottom-24 left-1/2 transform -translate-x-1/2">
+            <SketchPicker
+              color={selectedColor}
+              onChangeComplete={handleColorChange}
+              styles={pickerStyles}
+            />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
